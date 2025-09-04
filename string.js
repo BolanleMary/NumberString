536fb20d580bb75a371900a6ecc78b0dbd771cc8
Olowookere Bolanle Mary <bolanlemary9@gmail.com>
2025-09-04 19:37:43 +0100

    data strings

---
+++ string.js
@@ -0,0 +1,11 @@
+//strings are written/enclosed in " " or ' ' or ` `. EXAmple
+"i am here"
+'1000' //even though it is number but with the quote it is a string
+`today is beautiful`
+"it is Bola's" //to use a quote sign in the sentence
+`it is Bola's`
+'it is Bola\'s' //the back stroke is use to escape character
+`show the
+
+
+direction` //with bartick... i.e `` you allowed to miss a line(s) and this will write the code into another line
