6685688754eac0349837c2e1ce8bc2868b7fc9b9
Olowookere Bolanle Mary <bolanlemary9@gmail.com>
2025-09-08 11:30:47 +0100

    variable

---
+++ variable.js
@@ -0,0 +1,23 @@
+//Computer read from top to bottom and then gabbage it(forget it)
+//in order to catch or hold  the vaule we will put it it in a variable
+let age =30
+age = 14
+age =25
+let  ="wt226"
+
+//logical operator
+console.log(true && false) //and operator
+console.log(true || false) //or operator
+console.log(!false) //not operator
+
+//Data coerson
+console.log("5" - 1) //Js will think it is minus and 
+console.log ("5" + 1)//js will think you are trying to do concatenation
+console.log(false == 0)//true
+console.log(false === 0)//false
+
+let status;
+
+console.log(status)//undefined
+status = admin
+console.log(status)// admin
