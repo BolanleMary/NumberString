da7d7819ca857a5d4d10d20ee52c64394fd101e3
Olowookere Bolanle Mary <bolanlemary9@gmail.com>
2025-09-16 12:18:10 +0100

    forloop

---
+++ loop2.js
@@ -0,0 +1,30 @@
+//For loop example
+
+/*for(initializing, condition, change_segment){
+
+};*/
+
+
+for (let i =1; i<= 10; ++i){
+
+
+};
+
+for (let i =1; i<= 10; i= i+ 5){
+
+
+};
+
+let i = 0;
+
+console.log(i);
+console.log(++i ); //preIncrement. it is same as pre decrement --i
+console.log(i++ ) //post increment. it is same as post decrement i--
+
+
+let sum =0;
+
+for (let i =1; i <= 100; ++i){
+
+    sum =sum +i
+}
