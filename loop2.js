7323046dc2d7099919c62c8a7efc7e46ba8bfe7f
Olowookere Bolanle Mary <bolanlemary9@gmail.com>
2025-09-17 13:27:42 +0100

    for loop

--- loop2.js
+++ loop2.js
@@ -5,7 +5,7 @@
 };*/
 
 
-for (let i =1; i<= 10; ++i){
+/*for (let i =1; i<= 10; ++i){
 
 
 };
@@ -18,7 +18,7 @@ for (let i =1; i<= 10; i= i+ 5){
 let i = 0;
 
 console.log(i);
-console.log(++i ); //preIncrement. it is same as pre decrement --i
+console.log(++i, 'preIncrement' ); //preIncrement. it is same as pre decrement --i
 console.log(i++ ) //post increment. it is same as post decrement i--
 
 
@@ -28,3 +28,20 @@ for (let i =1; i <= 100; ++i){
 
     sum =sum +i
 }
+
+const names= ["diana", "franca", "temitope", "salome"]
+for (let j =0; j < names.length; j++){
+
+    names[j] =names[j].toUpperCase()
+}
+
+console.log(j)
+
+//while loop
+
+while(i <= 100){
+    sum = sum +i;
+    i++
+};
+
+console.log(sum);*/
---
+++ loop3.js
@@ -0,0 +1,8 @@
+//optional chaining
+//break
+//continue
+for(let i= 1; i<=10; i++){
+    if(i % 2 === 0)
+        continue;
+}
+console.log(i)
